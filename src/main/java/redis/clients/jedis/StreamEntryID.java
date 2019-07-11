ca67674d81eecd91a4237ae2f1377e787559d7a1
wangxiao <wangxiao@localhost.com>
2019-07-11 10:40:13 +0800

    To iterate successively,EntryID require a next method

--- src/main/java/redis/clients/jedis/StreamEntryID.java
+++ src/main/java/redis/clients/jedis/StreamEntryID.java
@@ -4,44 +4,44 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class StreamEntryID implements Comparable<StreamEntryID>, Serializable{
-  
+
   private static final long serialVersionUID = 1L;
 
   /**
-  * Should be used only with XADD  
-  * 
+  * Should be used only with XADD
+  *
   * <code>
-  * XADD mystream * field1 value1 
+  * XADD mystream * field1 value1
   * </code>
   */
   public static final StreamEntryID NEW_ENTRY = new StreamEntryID() {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @Override
     public String toString(){
       return "*";
     }
   };
-  
-  
+
+
   /**
-  * Should be used only with XGROUP CREATE  
-  * 
+  * Should be used only with XGROUP CREATE
+  *
   * <code>
   * XGROUP CREATE mystream consumer-group-name $
   * </code>
   */
   public static final StreamEntryID LAST_ENTRY = new StreamEntryID() {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @Override
     public String toString(){
       return "$";
     }
   };
-  
+
   /**
    * Should be used only with XREADGROUP
    * <code>
@@ -49,28 +49,28 @@ public class StreamEntryID implements Comparable<StreamEntryID>, Serializable{
    * </code>
    */
   public static final StreamEntryID UNRECEIVED_ENTRY = new StreamEntryID() {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @Override
     public String toString(){
       return ">";
     }
   };
-    
+
   private long time;
   private long sequence;
 
   public StreamEntryID() {
     this(0, 0L);
   }
-  
+
   public StreamEntryID(String id) {
-    String[] split = id.split("-");    
+    String[] split = id.split("-");
     this.time = Long.parseLong(split[0]);
     this.sequence = Long.parseLong(split[1]);
   }
-  
+
   public StreamEntryID(long time, long sequence) {
     this.time = time;
     this.sequence = sequence;
@@ -89,7 +89,7 @@ public class StreamEntryID implements Comparable<StreamEntryID>, Serializable{
     StreamEntryID other = (StreamEntryID) obj;
     return this.time == other.time && this.sequence == other.sequence;
   }
-  
+
   @Override
   public int hashCode() {
     return this.toString().hashCode();
@@ -108,14 +108,18 @@ public class StreamEntryID implements Comparable<StreamEntryID>, Serializable{
   public long getSequence() {
     return sequence;
   }
-  
+
   private void writeObject(java.io.ObjectOutputStream out) throws IOException{
     out.writeLong(this.time);
     out.writeLong(this.sequence);
   }
-  
+
   private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException{
     this.time = in.readLong();
     this.sequence = in.readLong();
+  }
+
+  public StreamEntryID getNext(){
+    return new StreamEntryID(time,sequence+1);
   }
 }
