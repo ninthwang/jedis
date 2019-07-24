674b74d3624115cf67339c360eb6a0782ed5e42e
wangxiao <wangxiao@localhost.com>
2019-07-24 09:52:38 +0800

    optimize import

--- src/main/java/redis/clients/jedis/BinaryClient.java
+++ src/main/java/redis/clients/jedis/BinaryClient.java
@@ -1,18 +1,20 @@
 package redis.clients.jedis;
 
-import static redis.clients.jedis.Protocol.Command.EXISTS;
-import static redis.clients.jedis.Protocol.Command.GET;
-import static redis.clients.jedis.Protocol.Command.KEYS;
-import static redis.clients.jedis.Protocol.Command.PING;
-import static redis.clients.jedis.Protocol.Command.PSUBSCRIBE;
-import static redis.clients.jedis.Protocol.Command.PUNSUBSCRIBE;
-import static redis.clients.jedis.Protocol.Command.SET;
-import static redis.clients.jedis.Protocol.Command.SUBSCRIBE;
-import static redis.clients.jedis.Protocol.Command.TIME;
-import static redis.clients.jedis.Protocol.Command.UNSUBSCRIBE;
-import static redis.clients.jedis.Protocol.Keyword.*;
 import static redis.clients.jedis.Protocol.toByteArray;
 import static redis.clients.jedis.Protocol.Command.*;
+import static redis.clients.jedis.Protocol.Keyword.ENCODING;
+import static redis.clients.jedis.Protocol.Keyword.IDLETIME;
+import static redis.clients.jedis.Protocol.Keyword.LEN;
+import static redis.clients.jedis.Protocol.Keyword.LIMIT;
+import static redis.clients.jedis.Protocol.Keyword.NO;
+import static redis.clients.jedis.Protocol.Keyword.ONE;
+import static redis.clients.jedis.Protocol.Keyword.REFCOUNT;
+import static redis.clients.jedis.Protocol.Keyword.RESET;
+import static redis.clients.jedis.Protocol.Keyword.STORE;
+import static redis.clients.jedis.Protocol.Keyword.WITHSCORES;
+import static redis.clients.jedis.Protocol.Keyword.STREAM;
+import static redis.clients.jedis.Protocol.Keyword.GROUPS;
+import static redis.clients.jedis.Protocol.Keyword.CONSUMERS;
 
 import java.util.ArrayList;
 import java.util.List;
